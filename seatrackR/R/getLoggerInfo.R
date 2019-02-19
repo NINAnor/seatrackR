ddc09214fae035de645c1ea7e183d743426bead4
Jens Åström <jens.astrom@nina.no>
2019-02-19 14:13:30 +0100

    added updatePosdataSession function. Updated getLoggerInfo to read from wider view

--- seatrackR/R/getLoggerInfo.R
+++ seatrackR/R/getLoggerInfo.R
@@ -1,7 +1,7 @@
-#' View the logger info table
+#' View the view info table
 #'
-#' This is a convenience function that reads from the table "loggers.logger_info"
-#'
+#' This is a convenience function that reads from the view "views.logger_info". Note that there also exists a table "loggers.logger_info" with more limited information.
+#' #'
 #' @return Data frame.
 #' @export
 #' @examples
@@ -13,7 +13,7 @@
 getLoggerInfo <- function(){
   checkCon()
 
-  DBI::dbGetQuery(con, "SELECT * FROM loggers.logger_info") %>% as_tibble()
+  DBI::dbReadTable(con, Id(schema = "views", table = "logger_info")) %>% as_tibble()
 }
 
 
