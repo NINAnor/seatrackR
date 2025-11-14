67ade1eb23547e8ae9beff17013ecfd3591b3b67
jevansbio <jevansbio@gmail.com>
2025-11-14 11:02:19 +0100

    Modify vignette generation

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -5,8 +5,7 @@ pkgdown_dict <- yaml::read_yaml("_pkgdown.yml")
 src_dir <- file.path("vignettes", "src")
 main_dir <- "vignettes"
 
-article_path <- file.path(main_dir, "articles")
-unlink(article_path)
+article_path <- main_dir
 dir.create(article_path, recursive = TRUE, showWarnings = FALSE)
 
 curr_menu <- pkgdown_dict$navbar$components$articles$menu <- list()
@@ -20,12 +19,11 @@ for (vignette in all_vignettes) {
     metadata <- rmarkdown::yaml_front_matter(curr_path)
     knitr::knit(curr_path, output = file.path(article_path, new_name))
     title <- metadata$title
-    pkgdown_dict$navbar$components$articles$menu[[title]] <- list(text = title, href = paste0("articles/articles/", gsub("Rmd", "html", new_name)))
+    pkgdown_dict$navbar$components$articles$menu[[title]] <- list(text = title, href = paste0("articles/", gsub("Rmd", "html", new_name)))
 }
 
-data_ref_path <- file.path(main_dir, "data_reference")
-unlink(data_ref_path)
-dir.create(data_ref_path, recursive = TRUE, showWarnings = FALSE)
+data_ref_path <- main_dir
+# dir.create(data_ref_path, recursive = TRUE, showWarnings = FALSE)
 
 # Copy yaml file into pkgdown assets folder
 asset_path <- file.path("pkgdown", "assets", "yaml")
@@ -48,7 +46,7 @@ for (key in names(schema_dict$datasets)) {
     knitr::knit(system.file("rmd", "table_ref_template.Rmd", package = "seatrackR"),
         output = file.path(data_ref_path, paste0(key, ".Rmd"))
     )
-    pkgdown_dict$navbar$components$data_ref$menu[[title]] <- list(text = title, href = paste0("articles/data_reference/", paste0(key, ".html")))
+    pkgdown_dict$navbar$components$data_ref$menu[[title]] <- list(text = title, href = paste0("articles/", paste0(key, ".html")))
 }
 
 yaml::write_yaml(pkgdown_dict, "_pkgdown.yml")
